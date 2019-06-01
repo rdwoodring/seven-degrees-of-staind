36ff5c8c3e334a31a002e3d44a211a197817c9fe
Ryan Woodring <rdwoodring@gmail.com>
2019-05-31 20:47:56 -0400

    #26 - pulling the Home container into the app file under the Home route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 
+import Home from './pages/home/Home';
+
 class App extends React.Component {
     render() {
         return (
@@ -8,7 +10,7 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path="/" render={() => {
                         return (
-                            <div>Home</div>
+                            <Home />
                         );
                     }} />
                 </Switch>
