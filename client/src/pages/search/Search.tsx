0e906e23dd3ca7ba4ea133dfc8e10d644d1e6093
Ryan Woodring <rdwoodring@gmail.com>
2019-06-18 21:23:56 -0400

    #32 - stubbing out the search container component

---
+++ client/src/pages/search/Search.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ISearchProps from './ISearchProps';
+
+import SearchPresentation from './SearchPresentation';
+
+class Search extends React.Component {
+    render() {
+        return (
+            <SearchPresentation />
+        );
+    }
+}
+
+export default Search;
