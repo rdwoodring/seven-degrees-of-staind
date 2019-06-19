beb442cd60040d1d3914a360944941031bed2790
Ryan Woodring <rdwoodring@gmail.com>
2019-06-18 21:23:30 -0400

    #33 - stubbing out the search page presentational component

---
+++ client/src/pages/search/SearchPresentation.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ISearchPresentationProps from './ISearchProps';
+
+class SearchPresentation extends React.Component {
+    render() {
+        return (
+            <div>
+                Search Presentation
+            </div>
+        )
+    }
+}
+
+export default SearchPresentation;
