2bdd145d6392299cd7b2e269f54246d48ccaeeec
Ryan Woodring <rdwoodring@gmail.com>
2019-06-20 21:45:54 -0400

    #37 - stub out the 403 presentation

---
+++ client/src/pages/403/FourOhThreePresentation.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import IFourOhThreePresentationProps from './IFourOhThreePresentationProps';
+
+class FourOhThreePresentation extends React.Component<IFourOhThreePresentationProps> {
+    render() {
+        return (
+            <div>
+                403
+            </div>
+        );
+    }
+}
+
+export default FourOhThreePresentation;
