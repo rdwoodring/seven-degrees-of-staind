6c2583d7d3f04099cf59d4503d8704db4431d889
Ryan Woodring <rdwoodring@gmail.com>
2019-06-20 21:56:32 -0400

    #31 - add the route for the search. wrap it in withIsLogged in and withMustBeLoggedInToView so that the user will be redirected away from the search page if they don't have an access token

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 
 import Home from './pages/home/Home';
+import Search from './pages/search/Search';
 import FourOhThree from './pages/403/FourOhThree';
+
+import withIsLoggedIn from './HOCs/withIsLoggedIn/withIsLoggedIn';
+import withMustBeLoggedInToView from './HOCs/withMustBeLoggedInToView/withMustBeLoggedInToView';
+
+const WrappedSearch = withIsLoggedIn(withMustBeLoggedInToView(Search));
 
 class App extends React.Component {
     render() {
@@ -12,6 +18,11 @@ class App extends React.Component {
                     <Route exact path="/" render={() => {
                         return (
                             <Home />
+                        );
+                    }} />
+                    <Route exact path="/search" render={() => {
+                        return (
+                            <WrappedSearch />
                         );
                     }} />
                     <Route exact path="/403" render={() => {
