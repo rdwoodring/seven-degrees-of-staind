3dc52a2fb7f7fc4a2e18a35f3bb9cddf24a4680e
Ryan Woodring <rdwoodring@gmail.com>
2019-06-27 21:58:51 -0400

    #51 - pulling the hero card into the home presentation

--- client/src/pages/home/HomePresentation.tsx
+++ client/src/pages/home/HomePresentation.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import IHomePresentationProps from './IHomePresentationProps';
 
+import withLoggedIn from '../../HOCs/withIsLoggedIn/withIsLoggedIn';
+
+import HeroCard from '../../components/cards/hero-card/HeroCard';
+
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
@@ -10,24 +14,14 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
+const WrappedHeroCard = withLoggedIn(HeroCard);
+
 class HomePresentation extends React.Component<IHomePresentationProps> {
     render() {
         return (
             <>
                 <div>
-                    <Card raised={true}>
-                        <CardHeader title={`Nothing "butt" rock!`} />
-                        <CardContent>
-                            <Typography variant="body1" gutterBottom>
-                                Login with your Spotify account to search for your favorite artists and see if they're butt rock or not
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="large" variant="contained" color="primary" onClick={this.props.handleClickHeroLoginButton}>
-                                Login to Spotify
-                            </Button>
-                        </CardActions>
-                    </Card>
+                    <WrappedHeroCard />
                 </div>
                 <br />
                 <div>
