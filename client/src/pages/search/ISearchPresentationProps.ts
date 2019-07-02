fad5a97b60b2314674f736e7c397a0cf0125a0ab
Ryan Woodring <rdwoodring@gmail.com>
2019-07-01 20:44:09 -0400

    #33 - first pass at the populated search page presentation

--- client/src/pages/search/ISearchPresentationProps.ts
+++ client/src/pages/search/ISearchPresentationProps.ts
@@ -1,1 +1,10 @@
-export default interface ISearchPresentationProps {};
+import {SyntheticEvent} from 'react';
+
+import ISearchState from './ISearchState';
+
+export default interface ISearchPresentationProps extends ISearchState {
+    search: string,
+    handleChangeSearchField(e: SyntheticEvent): void
+    handleKeyUpSearchField(e: SyntheticEvent): void
+    handleClickSearchButton(): void
+};
--- client/src/pages/search/SearchPresentation.tsx
+++ client/src/pages/search/SearchPresentation.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
 
-import ISearchPresentationProps from './ISearchProps';
+import ISearchPresentationProps from './ISearchPresentationProps';
 
-class SearchPresentation extends React.Component {
+import SearchWithButton from '../../components/searches/search-with-button/SearchWithButton';
+import ArtistItemCard from '../../components/cards/artist-item-card/ArtistItemCard';
+
+class SearchPresentation extends React.Component<ISearchPresentationProps> {
     render() {
+        const artistMarkup = this.buildArtistItemCards();
+
         return (
             <div>
-                Search Presentation
+                <SearchWithButton
+                    search={this.props.artist}
+                    handleChangeSearchField={this.props.handleChangeSearchField}
+                    handleKeyUpSearchField={this.props.handleKeyUpSearchField}
+                    handleClickSearchButton={this.props.handleClickSearchButton}
+                />
+                <div>
+                    {artistMarkup}
+                </div>
             </div>
         )
+    }
+
+    private buildArtistItemCards() {
+        return this.props.results.map((item) => {
+            return (
+                <div key={item.id} style={{
+                    marginBottom: '5px'
+                }}>
+                    <ArtistItemCard {...item} />
+                </div>
+            );
+        });
     }
 }
 
