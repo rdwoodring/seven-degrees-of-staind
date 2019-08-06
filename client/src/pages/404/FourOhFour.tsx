a67959e2717783b7f7b8a8ef2e3de6054ee9b74f
Ryan Woodring <rdwoodring@gmail.com>
2019-08-05 21:04:23 -0400

    #87 - create a 404 container

---
+++ client/src/pages/404/FourOhFour.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import IFourOhFourProps from './IFourOhFourProps';
+
+import FourOhFourPresentation from './FourOhFourPresentation';
+
+class FourOhFour extends React.Component<IFourOhFourProps> {
+    render() {
+        return (
+            <FourOhFourPresentation />
+        );
+    }
+}
+
+export default FourOhFour
