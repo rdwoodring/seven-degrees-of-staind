a02c0e02c4b10921f52ce91b83c7f5c27a8165cd
Ryan Woodring <rdwoodring@gmail.com>
2019-08-14 20:22:14 -0400

    #82 - wrapping the 404 and 403 presentations in a card and box

--- client/src/pages/403/FourOhThreePresentation.tsx
+++ client/src/pages/403/FourOhThreePresentation.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Card from '@material-ui/core/Card';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
@@ -8,22 +10,24 @@ import IFourOhThreePresentationProps from './IFourOhThreePresentationProps';
 class FourOhThreePresentation extends React.Component<IFourOhThreePresentationProps> {
     render() {
         return (
-            <div>
-                <Typography variant="h1" align="center">
-                    403
-                </Typography>
-                <Typography variant="h2" align="center">
-                    Access Denied
-                </Typography>
-                <Typography variant="body1" align="center">
-                    <div>
-                        You must login with your Spotify account to see this page.
-                    </div>
-                    <Button size="large" variant="contained" color="primary" onClick={this.props.handleClickLoginButton}>
-                        Login to Spotify
-                    </Button>
-                </Typography>
-            </div>
+            <Card raised={true}>
+                <Box p={2}>
+                    <Typography variant="h1" component="h2" align="center">
+                        403
+                    </Typography>
+                    <Typography variant="h2" component="h3" align="center">
+                        Access Denied
+                    </Typography>
+                    <Typography variant="body1" align="center">
+                        <div>
+                            You must login with your Spotify account to see this page.
+                        </div>
+                        <Button size="large" variant="contained" color="primary" onClick={this.props.handleClickLoginButton}>
+                            Login to Spotify
+                        </Button>
+                    </Typography>
+                </Box>
+            </Card>
         );
     }
 }
--- client/src/pages/404/FourOhFourPresentation.tsx
+++ client/src/pages/404/FourOhFourPresentation.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Card from '@material-ui/core/Card';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
@@ -8,14 +10,16 @@ import IFourOhFourPresentationProps from './IFourOhFourPresentationProps';
 class FourOhFourPresentation extends React.Component<IFourOhFourPresentationProps> {
     render() {
         return (
-            <div>
-                <Typography variant="h1" align="center">
-                    404
-                </Typography>
-                <Typography variant="h2" align="center">
-                    Page Not Found
-                </Typography>
-            </div>
+            <Card raised={true}>
+                <Box p={2}>
+                    <Typography variant="h1" component="h2" align="center">
+                        404
+                    </Typography>
+                    <Typography variant="h2" component="h3" align="center">
+                        Page Not Found
+                    </Typography>
+                </Box>
+            </Card>
         );
     }
 }
