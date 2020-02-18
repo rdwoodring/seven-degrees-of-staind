fc265de5e61016d147a1aca9b5c9ea1a69701ddd
Ryan Woodring <rdwoodring@gmail.com>
2020-02-17 22:07:03 -0500

    #129 - clean up uses of deprecated Buffer constructor in login.ts and swap them over to Buffer.from() instead

--- server/src/routes/login.ts
+++ server/src/routes/login.ts
@@ -114,7 +114,7 @@ router.get('/login/callback', function(req, res) {
                 grant_type: 'authorization_code'
             },
             headers: {
-                'Authorization': 'Basic ' + (new Buffer(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'))
+                'Authorization': 'Basic ' + (Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'))
             },
             json: true
         };
@@ -165,7 +165,7 @@ router.get('/login/refresh_token', function(req, res) {
   var refresh_token = req.query.refresh_token;
   var authOptions = {
     url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')) },
+    headers: { 'Authorization': 'Basic ' + (Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')) },
     form: {
       grant_type: 'refresh_token',
       refresh_token: refresh_token
