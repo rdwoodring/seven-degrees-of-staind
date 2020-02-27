2e9ed79d14ecd530be366d426c04bc0403a3f00d
Ryan Woodring <rdwoodring@gmail.com>
2020-02-26 22:13:35 -0500

    #125 - clean up some styling stuff around NavbarPresentationa nd the public index.html template. add some proper tests to the App file

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { configure, shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import Adapter from 'enzyme-adapter-react-16';
+
+import { Route, BrowserRouter, Switch, Redirect } from "react-router-dom";
+
+
+import App, {
+    WrappedNavbar,
+    WrappedSearch
+} from './App';
+
+import Footer from './components/footers/Footer';
+
+configure({ adapter: new Adapter() });
+
+describe('render', () => {
+    it('should render a WrappedNavbar', () => {
+        const wrapper = shallow<App>(<App />);
+
+        expect(wrapper.find(WrappedNavbar).length).toBe(1);
+    });
+
+    it('should render a BrowserRouter', () => {
+        const wrapper = shallow<App>(<App />);
+
+        expect(wrapper.find(BrowserRouter).length).toBe(1);
+    });
+
+    it('should render a Footer', () => {
+        const wrapper = shallow<App>(<App />);
+
+        expect(wrapper.find(Footer).length).toBe(1);
+    });
 });
