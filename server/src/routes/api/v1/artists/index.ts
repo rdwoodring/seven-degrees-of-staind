2a9a611b8f0e42385a16fb347976344dd2bf13b6
Ryan Woodring <rdwoodring@gmail.com>
2020-08-07 22:01:19 -0400

    #144 - use the artists handler in an artists router and attach the route in the v1 urlspace

---
+++ server/src/routes/api/v1/artists/index.ts
@@ -0,0 +1,51 @@
+import {
+    Router,
+    Request,
+    Response,
+    NextFunction
+} from 'express';
+
+import IExpressError from '../../../../errors/IExpressError';
+import ErrorCodes from '../../../../errors/ErrorCodes';
+
+import {
+    handleSearch
+} from '../../../../spotify/spotify-api-handlers/searchHandler';
+
+import {
+    handleArtists
+} from '../../../../spotify/spotify-api-handlers/artistsHandler';
+
+const router: Router = Router();
+
+router.get('/artists', function(req: Request, res: Response, next: NextFunction) {
+    if (!req.query.search) {
+        const err = new Error('A search query parameter is required. Try appending ?search=yoursearchterm and retrying the request') as IExpressError;
+
+        err.status = ErrorCodes.BadRequest;
+
+        throw err;
+    }
+
+    handleSearch(req.query.artist as string, req.session!.accessToken).then((data) => {
+            res.json(data);
+        })
+        .catch((err: IExpressError) => {
+            err.status = ErrorCodes.InternalServerError;
+
+            next(err);
+        });
+});
+
+router.get('/artists/:artistId', function(req: Request, res: Response, next: NextFunction) {
+    handleArtists(req.params.artistId, req.session!.accessToken).then((data) => {
+            res.json(data);
+        })
+        .catch((err: IExpressError) => {
+            err.status = ErrorCodes.InternalServerError;
+
+            next(err);
+        });
+});
+
+export default router;
--- server/src/routes/api/v1/v1.ts
+++ server/src/routes/api/v1/v1.ts
@@ -3,9 +3,11 @@ import {
 } from 'express';
 
 import searchRouter from './search';
+import artistsRouter from './artists';
 
 const router: Router = Router();
 
 router.use('/v1', searchRouter);
+router.use('/v1', artistsRouter);
 
 export default router;
